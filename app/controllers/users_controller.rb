6fc33e9676759df01c8f30e25d6e8b15565fd6d4
Rebecca <rfjackson11@gmail.com>
2015-09-06 21:34:27 -0600

    Fix weird lowercasing that happened on last commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
-      flash[:info] = "please check your email to activate your account."
+      flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
       render 'new'
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
 
   def update
     if @user.update_attributes(user_params)
-      flash[:success] = "profile updated"
+      flash[:success] = "Profile updated"
       redirect_to @user
     else
       render 'edit'
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
 
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "user deleted"
+    flash[:success] = "User deleted"
     redirect_to users_url
   end
 
@@ -51,24 +51,24 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
-  #before filters
+  #Before filters
 
-  #confirms a logged-in user.
+  #Confirms a logged-in user.
   def logged_in_user
     unless logged_in?
       store_location
-      flash[:danger] = "please log in."
+      flash[:danger] = "Please log in."
       redirect_to login_url
     end
   end
 
-  # confirms the correct user.
+  # Confirms the correct user.
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
   end
 
-  # confirms an admin user.
+  # Confirms an admin user.
   def admin_user
     redirect_to(root_url) unless current_user.admin?
   end
